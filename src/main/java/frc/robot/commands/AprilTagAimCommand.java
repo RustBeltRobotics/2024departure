f57025c6395c283cf1238fc5e76e6949a98df773
cicada7772 <45969223+cicada7772@users.noreply.github.com>
2024-01-23 14:33:33 -0500

    Update AprilTagAimCommand.java

--- src/main/java/frc/robot/commands/AprilTagAimCommand.java
+++ src/main/java/frc/robot/commands/AprilTagAimCommand.java
@@ -3,16 +3,22 @@ package frc.robot.commands;
 import static frc.robot.Constants.limelightName;
 import static frc.robot.Constants.rotation_P;
 
+import java.util.Optional;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.Drivetrain;
     public class AprilTagAimCommand extends Command {
-
+    private double tx;
     private String target;
-    private double tx = LimelightHelpers.getTX(limelightName);
+    private double sightedTID;
+    private double targetTID;
+    private double targetTID2 = -1;
     private double steeringAdjust; 
+    private Optional<Alliance> alliance = DriverStation.getAlliance();
 
     private final Drivetrain drivetrain;
     public AprilTagAimCommand(Drivetrain drivetrain, String target) {
@@ -22,23 +28,48 @@ import frc.robot.subsystems.Drivetrain;
     }
     @Override
     public void execute() {
-        switch (target) {
-            case "speaker":
-                //setPipelineIndex(limelightName, 0);
-                break;
-            case "amp":
-                //setPipelineIndex(limelightName, 1);
-                break;
-            case "source":
-                //setPipelineIndex(limelightName, 2);
-                break;
+        if (alliance.isPresent()) {
+            if (alliance.get() == DriverStation.Alliance.Red) {
+                switch (target) {
+                case "speaker":
+                    targetTID = 3;
+                    targetTID2 = 4;
+                    break;
+                case "amp":
+                    targetTID = 5;
+                    break;
+                case "source":
+                    targetTID = 9;
+                    targetTID2 = 10;                    
+                    break;
+                }
+            }
+        } else {//its blue
+                switch (target) {
+                case "speaker":
+                    targetTID = 7;
+                    targetTID2 = 8;
+                    break;
+                case "amp":
+                    targetTID = 6;
+                    break;
+                case "source":
+                    targetTID = 1;
+                    targetTID2 = 2;
+                    break;
+                }
         }
-        steeringAdjust = rotation_P * tx;
-        drivetrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
-            0,
-            0,
-            steeringAdjust,
-            Rotation2d.fromDegrees(drivetrain.getGyroscopeAngle() + drivetrain.getGyroOffset())));
+        //determine if the primary tag id matches our target tag id
+        sightedTID = LimelightHelpers.getFiducialID(limelightName);
+        if (sightedTID == targetTID || sightedTID == targetTID2) {
+            tx = LimelightHelpers.getTX(limelightName);
+            steeringAdjust = rotation_P * tx;
+            drivetrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
+                0,
+                0,
+                steeringAdjust,
+                Rotation2d.fromDegrees(drivetrain.getGyroscopeAngle() + drivetrain.getGyroOffset())));
+        }
     }
     @Override
     public void end(boolean interrupted) {
